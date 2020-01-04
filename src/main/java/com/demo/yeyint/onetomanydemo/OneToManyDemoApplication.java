b68f00643937ca4269c386c3ee6a455001b90c65
Ye Yint <yeyient242016@gmail.com>
2020-01-04 11:05:52 +0630

    Initial Commit

---
+++ src/main/java/com/demo/yeyint/onetomanydemo/OneToManyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.yeyint.onetomanydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToManyDemoApplication.class, args);
+    }
+
+}
